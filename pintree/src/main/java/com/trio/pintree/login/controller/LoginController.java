9505ec53adf2b0411c956ea8686a74c738a8225a
sanhee <68936@naver.com>
2021-08-19 05:23:20 +0900

    feat : KaKao 엑세스 토큰 발급 컨트롤러 구현

--- pintree/src/main/java/com/trio/pintree/login/controller/LoginController.java
+++ pintree/src/main/java/com/trio/pintree/login/controller/LoginController.java
@@ -1,11 +1,42 @@
 package com.trio.pintree.login.controller;
 
+import com.trio.pintree.login.component.OauthServiceFactory;
+import com.trio.pintree.login.domain.Member;
+import com.trio.pintree.login.dto.AuthRequest;
+import com.trio.pintree.login.dto.AuthResponse;
+import com.trio.pintree.login.dto.KaKaoAccessTokenResponse;
+import com.trio.pintree.login.service.KakaoOauthService;
 import com.trio.pintree.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/login")
 public class LoginController {
     private final LoginService loginService;
+
+    @GetMapping("/kakao")
+    public ResponseEntity<AuthResponse> auth(@RequestBody AuthRequest authRequest) {
+        String code = authRequest.getCode();
+
+        OauthServiceFactory oauthServiceFactory = loginService.getOauthServiceFactory();
+        KakaoOauthService kakaoOauthService = oauthServiceFactory.getKaKaoOauthService();
+
+        KaKaoAccessTokenResponse accessTokenResponse = kakaoOauthService.issueAccessToken(code);
+
+        //Member member = kakaoOauthService.getMemberFrom(accessTokenResponse);
+
+        return ResponseEntity.status(CREATED)
+                             .body(new AuthResponse(accessTokenResponse.getAccessToken()));
+    }
+
 }
