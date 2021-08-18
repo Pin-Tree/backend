f8ff065188a2ad3e6801ba9eb02e51475f7c0c8c
pbg0205 <pbg0205@naver.com>
2021-08-18 17:10:42 +0900

    feat : Login 관련 Component 의존성 추가

--- pintree/src/main/java/com/trio/pintree/login/service/LoginService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/LoginService.java
@@ -1,7 +1,11 @@
 package com.trio.pintree.login.service;
 
+import com.trio.pintree.login.component.OauthServiceFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+    private final OauthServiceFactory oauthServiceFactory;
 }
