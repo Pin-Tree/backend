f64dcbb2189fceb3bc99bd20c452380faacce79b
pbg0205 <pbg0205@naver.com>
2021-08-18 15:51:04 +0900

    feat : 프로젝트 생성

---
+++ pintree/src/main/java/com/trio/pintree/PintreeApplication.java
@@ -0,0 +1,13 @@
+package com.trio.pintree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PintreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PintreeApplication.class, args);
+    }
+
+}
---
+++ pintree/src/test/java/com/trio/pintree/PintreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trio.pintree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PintreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
