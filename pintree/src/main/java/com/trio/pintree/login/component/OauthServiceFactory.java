1cc66a056a7a7800c4de5ba216307bf866e28dc1
sanhee <68936@naver.com>
2021-08-19 05:22:53 +0900

    feat : KaKao 엑세스 토큰 발급 로직 구현

--- pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
+++ pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
@@ -1,13 +1,25 @@
 package com.trio.pintree.login.component;
 
+import com.trio.pintree.login.service.KakaoOauthService;
 import com.trio.pintree.login.service.OauthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class OauthServiceFactory {
+    private final static String KAKAO_OAUTH_SERVICE_NAME = "kakaoOauthService";
     private final Map<String, OauthService> oauthServiceMap;
+
+    public OauthServiceFactory(Map<String, OauthService> oauthServiceMap) {
+        this.oauthServiceMap = oauthServiceMap;
+        log.debug("oauthServiceMap : {}", oauthServiceMap);
+    }
+
+    public KakaoOauthService getKaKaoOauthService() {
+        return (KakaoOauthService) oauthServiceMap.get(KAKAO_OAUTH_SERVICE_NAME);
+    }
 }
