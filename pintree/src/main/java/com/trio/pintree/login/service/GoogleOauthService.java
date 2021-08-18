378488cc0fcec618a110332bac8f75538c37ee64
pbg0205 <pbg0205@naver.com>
2021-08-18 17:10:42 +0900

    feat : Login 관련 Component 의존성 추가

--- pintree/src/main/java/com/trio/pintree/login/service/GoogleOauthService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/GoogleOauthService.java
@@ -2,10 +2,15 @@ package com.trio.pintree.login.service;
 
 import com.trio.pintree.login.domain.Member;
 import com.trio.pintree.login.dto.AccessTokenResponse;
+import com.trio.pintree.login.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GoogleOauthService implements OauthService {
+
+    private final MemberRepository memberRepository;
 
     @Override
     public AccessTokenResponse issueAccessToken(String... str) {
