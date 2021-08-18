8baa5e7a5ccf32dee9ba4f48bb77f705583be6af
pbg0205 <pbg0205@naver.com>
2021-08-18 16:55:59 +0900

    feat : OauthService interface 생성

--- pintree/src/main/java/com/trio/pintree/login/service/OauthService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/OauthService.java
@@ -1,4 +1,10 @@
 package com.trio.pintree.login.service;
 
+import com.trio.pintree.login.domain.Member;
+import com.trio.pintree.login.dto.AccessTokenResponse;
+
 public interface OauthService {
+    AccessTokenResponse issueAccessToken(String... str);
+
+    Member getMemberFrom(AccessTokenResponse accessTokenResponse);
 }
