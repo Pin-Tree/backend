6e3d4d3e2809795d203213ccfe2541ef94d04a51
pbg0205 <pbg0205@naver.com>
2021-08-18 16:57:12 +0900

    feat : KakaoOauthService 생성

--- pintree/src/main/java/com/trio/pintree/login/service/KakaoOauthService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/KakaoOauthService.java
@@ -1,7 +1,19 @@
 package com.trio.pintree.login.service;
 
+import com.trio.pintree.login.domain.Member;
+import com.trio.pintree.login.dto.AccessTokenResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KakaoOauthService implements OauthService {
+
+    @Override
+    public AccessTokenResponse issueAccessToken(String... str) {
+        return null;
+    }
+
+    @Override
+    public Member getMemberFrom(AccessTokenResponse accessTokenResponse) {
+        return null;
+    }
 }
