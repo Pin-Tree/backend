ce7445707259709aa6e3624f715ffccb6cfa96dc
sanhee <68936@naver.com>
2021-08-19 05:19:59 +0900

    feat : DTO 정의 (AuthRequest, AuthResponse, KaKaoAccessTokenResponse)

---
+++ pintree/src/main/java/com/trio/pintree/login/dto/KaKaoAccessTokenResponse.java
@@ -0,0 +1,29 @@
+package com.trio.pintree.login.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class KaKaoAccessTokenResponse implements AccessTokenResponse{
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private String expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    private String refreshTokenExpiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+}
