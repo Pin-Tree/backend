ee1c1e72969587d3e49104ba02b4d79f22132928
pbg0205 <pbg0205@naver.com>
2021-08-18 16:55:29 +0900

    feat : OauthServiceFactory 생성

--- pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
+++ pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
@@ -1,7 +1,13 @@
 package com.trio.pintree.login.component;
 
+import com.trio.pintree.login.service.OauthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@RequiredArgsConstructor
 public class OauthServiceFactory {
+    private final Map<String, OauthService> oauthServiceMap;
 }
