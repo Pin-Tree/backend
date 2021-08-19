2de42d75d5348915de0f4330a9d9fb456d668f8f
woody <woojihye2339@gmail.com>
2021-08-19 16:03:58 +0900

    [#4] feat : 네이버 로그인 인터셉터 구현
    > NaverOauthInterceptor
      - state 생성 후, session에 추가
    
    > OauthConfig
      - NaverOauthInterceptor 인터셉터 추가
      - /login/naver url 요청에 한해 작동

---
+++ pintree/src/main/java/com/trio/pintree/login/component/OauthConfig.java
@@ -0,0 +1,22 @@
+package com.trio.pintree.login.component;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OauthConfig implements WebMvcConfigurer {
+
+    private final NaverOauthInterceptor oauthInterceptor;
+
+    public OauthConfig(NaverOauthInterceptor oauthInterceptor) {
+        this.oauthInterceptor = oauthInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(oauthInterceptor)
+                .addPathPatterns("/login/naver");
+    }
+}
+
