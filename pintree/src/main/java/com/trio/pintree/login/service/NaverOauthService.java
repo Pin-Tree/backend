c3c76054d7a036c984aba9021c07953152c152ad
woody <woojihye2339@gmail.com>
2021-08-19 17:15:51 +0900

    [#4] feat : NaverOauthService 구현
    > NaverOauthService::issueAccessToken 구현
      - webClient로 access token 요청 후 응답을 컨트롤러 계층으로 리턴

--- pintree/src/main/java/com/trio/pintree/login/service/NaverOauthService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/NaverOauthService.java
@@ -1,20 +1,37 @@
 package com.trio.pintree.login.service;
 
+import com.trio.pintree.login.component.NaverOauthProperties;
 import com.trio.pintree.login.domain.Member;
 import com.trio.pintree.login.dto.AccessTokenResponse;
+import com.trio.pintree.login.dto.NaverAccessTokenResponse;
 import com.trio.pintree.login.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class NaverOauthService implements OauthService {
 
+    private final NaverOauthProperties oauthProperties;
     private final MemberRepository memberRepository;
+    private final WebClient webClient;
 
     @Override
     public AccessTokenResponse issueAccessToken(String... str) {
-        return null;
+        String code = str[0];
+        String state = str[1];
+
+        String uri = oauthProperties.getAsUriParams(code, state);
+
+        return webClient.get()
+                .uri(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(NaverAccessTokenResponse.class)
+                .blockOptional()
+                .orElseThrow(RuntimeException::new);
     }
 
     @Override
