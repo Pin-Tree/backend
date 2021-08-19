e2aa77dd03fcaf506565e0ff05dce15303a4bc64
woody <woojihye2339@gmail.com>
2021-08-19 17:28:04 +0900

    [#4] feat : OauthServiceFactory 구현
    > OauthService 구현체 관리 클래스
      - Map 형태로 모든 구현체들을 가지고 있음
    
    > OauthServiceFactory::getNaverOauthService
      - NaverOauthService 구현체 리턴

--- pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
+++ pintree/src/main/java/com/trio/pintree/login/component/OauthServiceFactory.java
@@ -9,5 +9,11 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class OauthServiceFactory {
+    private final static String NAVER_OAUTH_SERVICE_NAME = "naverOauthService";
+
     private final Map<String, OauthService> oauthServiceMap;
+
+    public OauthService getNaverOauthService() {
+        return oauthServiceMap.get(NAVER_OAUTH_SERVICE_NAME);
+    }
 }
