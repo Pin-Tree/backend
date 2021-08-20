75c0f2b37dbb3d16ae31f4cc16ae81bbc979b36b
sanhee <68936@naver.com>
2021-08-20 14:34:33 +0900

    fix: 아직까지 불필요한 HttpClient 설정 제거

--- pintree/src/main/java/com/trio/pintree/WebConfig.java
+++ pintree/src/main/java/com/trio/pintree/WebConfig.java
@@ -1,38 +1,18 @@
 package com.trio.pintree;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import reactor.netty.http.client.HttpClient;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    public HttpClient httpClient() {
-        return HttpClient.create()
-                         .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
-                         .responseTimeout(Duration.ofMillis(5000))
-                         .doOnConnected(
-                                 conn -> conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                                             .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS))
-                         );
-    }
-
-    @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                        .clientConnector(new ReactorClientHttpConnector(httpClient()))
                         .build();
     }
 }
