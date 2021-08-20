765d8f07a72569a05bca9b80342eff326c921fbc
pbg0205 <pbg0205@naver.com>
2021-08-20 19:03:49 +0900

    refactor : resource server 요청 방식 변경
    - ResTemplate -> WebClient방식으로 변경

--- pintree/src/main/java/com/trio/pintree/login/service/KakaoOauthService.java
+++ pintree/src/main/java/com/trio/pintree/login/service/KakaoOauthService.java
@@ -8,11 +8,9 @@ import com.trio.pintree.login.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
@@ -20,58 +18,47 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class KakaoOauthService implements OauthService {
 
-    private static final RestTemplate restTemplate = new RestTemplate();
     private final MemberRepository memberRepository;
     private final WebClient webClient;
     private final KaKaoOauthProperties kaKaoOauthProperties;
 
     @Override
     public KaKaoAccessTokenResponse issueAccessToken(String... str) {
-
         final String code = str[0];
 
-        MultiValueMap<String, String> bodies = new LinkedMultiValueMap<>();
-        bodies.add("grant_type", kaKaoOauthProperties.getGrantType());
-        bodies.add("client_id", kaKaoOauthProperties.getClientId());
-        bodies.add("redirect_uri", kaKaoOauthProperties.getRedirectUri());
-        bodies.add("code", code);
-        bodies.add("client_secret", kaKaoOauthProperties.getSecretKey());
-
-        RequestEntity<MultiValueMap<String, String>> kakaoAccessTokenRequest = RequestEntity
-                .post(kaKaoOauthProperties.getAccessTokenUri()) // status line
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED) // header
-                .body(bodies);
+        MultiValueMap<String, String> kakaoAccessTokenRequest = generateAccessTokenRequest(code);
 
         log.debug("accessTokenRequest : {}", kakaoAccessTokenRequest);
 
         KaKaoAccessTokenResponse kaKaoAccessTokenResponse = sendRequestForAccessToken(kakaoAccessTokenRequest);
 
-        log.debug("AccessToken : {}", kaKaoAccessTokenResponse.getAccessToken());
+        log.debug("kaKaoAccessTokenResponse : {}", kaKaoAccessTokenResponse);
 
         return kaKaoAccessTokenResponse;
     }
 
-    private KaKaoAccessTokenResponse sendRequestForAccessToken(RequestEntity<MultiValueMap<String, String>> kakaoAccessTokenRequest) {
-        try {
-            return restTemplate.exchange(
-                    kakaoAccessTokenRequest,
-                    KaKaoAccessTokenResponse.class
-            ).getBody();
-        } catch (Exception e) {
-            throw new RuntimeException("엑세스 토큰 획득 실패 "+e.getMessage());
-        }
-    }
-
-    // 동작 안하는 메서드
-    private KaKaoAccessTokenResponse sendRequestForAccessToken_(RequestEntity<MultiValueMap<String, String>> kakaoAccessTokenRequest) {
+    private KaKaoAccessTokenResponse sendRequestForAccessToken(MultiValueMap<String, String> kakaoAccessTokenRequest) {
         return webClient.post()
                 .uri(kaKaoOauthProperties.getAccessTokenUri())
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
                 .bodyValue(kakaoAccessTokenRequest)
                 .retrieve()
                 .bodyToMono(KaKaoAccessTokenResponse.class)
                 .blockOptional()
                 .orElseThrow(RuntimeException::new);
+    }
+
+    private MultiValueMap<String, String> generateAccessTokenRequest(String code) {
+        MultiValueMap<String, String> accessTokenRequestMap = new LinkedMultiValueMap<>();
+
+        accessTokenRequestMap.add("grant_type", kaKaoOauthProperties.getGrantType());
+        accessTokenRequestMap.add("client_id", kaKaoOauthProperties.getClientId());
+        accessTokenRequestMap.add("redirect_uri", kaKaoOauthProperties.getRedirectUri());
+        accessTokenRequestMap.add("code", code);
+        accessTokenRequestMap.add("client_secret", kaKaoOauthProperties.getSecretKey());
+
+        return accessTokenRequestMap;
     }
 
     @Override
