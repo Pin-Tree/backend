ca34027f9681e3666516629708823b77871d126e
sanhee <68936@naver.com>
2021-08-26 03:43:30 +0900

    feat : JWT 디코드 메서드 정의

--- pintree/src/test/java/com/trio/pintree/login/util/JwtUtilTest.java
+++ pintree/src/test/java/com/trio/pintree/login/util/JwtUtilTest.java
@@ -14,19 +14,24 @@ class JwtUtilTest {
 
     @Test
     void uuid를_페이로드로_JWT_생성에_성공한다() throws Exception {
+        // given
         String uuid = "uuid";
+
+        // when
         String jwt = JwtUtil.createJwt(uuid);
+
+        // then
         String expectedJwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqd3RJc3N1ZXIiLCJ1dWlkIjoidXVpZCJ9.EFC8iPNxtnqz0AUibGyM55m7MXjIOH-WUmkeZ_vmSWs";
         assertThat(jwt).isEqualTo(expectedJwt);
     }
 
     @ParameterizedTest
-    @MethodSource("uuid가_비어있다면_JWT_생성은_실패한다")
-    void uuid가_비어있다면_JWT_생성은_실패한다(String uuid, String expectedMessage) {
+    @MethodSource("UUID가_비어있다면_JWT_생성은_실패한다")
+    void UUID가_비어있다면_JWT_생성은_실패한다(String uuid, String expectedMessage) {
         assertThatThrownBy(() -> JwtUtil.createJwt(uuid)).hasMessageContaining(expectedMessage);
     }
 
-    static Stream<Arguments> uuid가_비어있다면_JWT_생성은_실패한다() {
+    static Stream<Arguments> UUID가_비어있다면_JWT_생성은_실패한다() {
         return Stream.of(
                 Arguments.arguments(
                         "",
@@ -38,4 +43,39 @@ class JwtUtilTest {
                 )
         );
     }
+
+    @Test
+    void 유효한_JWT의_페이로드_디코드_성공(){
+        // given
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqd3RJc3N1ZXIiLCJ1dWlkIjoidXVpZCJ9.EFC8iPNxtnqz0AUibGyM55m7MXjIOH-WUmkeZ_vmSWs";
+
+        // when
+        String uuid = JwtUtil.decodeJwt(token);
+
+        // then
+        String expectedUuid = "uuid";
+        assertThat(uuid).isEqualTo(expectedUuid);
+    }
+
+    @ParameterizedTest
+    @MethodSource("JWT_디코드_실패")
+    void JWT_디코드_실패(String desc, String token, String expectedMessage) {
+        assertThatThrownBy(() -> JwtUtil.decodeJwt(token)).as(desc).hasMessageContaining(expectedMessage);
+    }
+
+    static Stream<Arguments> JWT_디코드_실패() {
+        return Stream.of(
+                Arguments.arguments(
+                        "Issuer가 잘못된 경우",
+                        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3cm9uZ0lzc3VlciIsInV1aWQiOiJ1dWlkIn0.LcliiAv4Of4BP1RtZ1CMevkxyi1CiJz1na97F00Uq-A",
+                        "JWT 검증 실패"
+                ),
+                Arguments.arguments(
+                        "JWT 형식이 잘못된 경우",
+                        "wrongJWT",
+                        "JWT 디코드 실패"
+                )
+        );
+    }
+
 }
