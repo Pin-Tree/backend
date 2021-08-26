511735b9fa9578e706495770b16b2fff66f376bf
pbg0205 <pbg0205@naver.com>
2021-08-26 16:45:18 +0900

    refactor : JwtResponse 생성자 및 정적 메서드 리팩토링

--- pintree/src/main/java/com/trio/pintree/login/dto/JwtResponse.java
+++ pintree/src/main/java/com/trio/pintree/login/dto/JwtResponse.java
@@ -1,22 +1,15 @@
 package com.trio.pintree.login.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE, staticName = "create")
 public class JwtResponse {
-    private static String DEFAULT_PROFILE_URL = "https://issue-tracker-img-storage.s3.ap-northeast-2.amazonaws.com/Screen+Shot+2021-08-25+at+4.28.33+PM.png";
 
     private final String jwt;
     private final String nickname;
     private final String profile;
 
-    private JwtResponse(String jwt, String nickname, String profile) {
-        this.jwt = jwt;
-        this.nickname = nickname;
-        this.profile = (profile != null) ? profile : DEFAULT_PROFILE_URL;
-    }
-
-    public static JwtResponse create(String jwt, String nickname, String profile){
-        return new JwtResponse(jwt, nickname, profile);
-    }
 }
