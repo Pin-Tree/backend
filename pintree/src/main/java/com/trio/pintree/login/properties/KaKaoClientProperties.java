d967841e69cda2e3134cd733e7fe5e16881f3ffc
woody <woojihye2339@gmail.com>
2021-08-26 16:14:58 +0900

    [#15] refactor : ClientController 통합
    > RedirectController::kakaoRedirect 로직 이동
    
    > 설정 정보 클래스 단위로 주입
      - NaverClientProperties 구현
      - KaKaoClientProperties 구현
    
    > root url 변경
      - /auth/social/redirect

---
+++ pintree/src/main/java/com/trio/pintree/login/properties/KaKaoClientProperties.java
@@ -0,0 +1,16 @@
+package com.trio.pintree.login.properties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "oauth.kakao")
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class KaKaoClientProperties {
+    private final String url;
+}
