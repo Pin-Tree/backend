02b5d0e96af2a978c2730adfbf3527aeab633a48
woody <woojihye2339@gmail.com>
2021-09-01 20:42:17 +0900

    feature : OauthConfig에 LoginInterceptor 추가
    > 로그인에 대한 인터셉터, LoginInterceptor 추가
      - /api/login/{google, naver, kakao} url에 대해 적용
    
    issue : #21

--- pintree/src/main/java/com/trio/pintree/login/config/OauthConfig.java
+++ pintree/src/main/java/com/trio/pintree/login/config/OauthConfig.java
@@ -1,6 +1,8 @@
 package com.trio.pintree.login.config;
 
 import com.trio.pintree.login.interceptor.NaverOauthInterceptor;
+import com.trio.pintree.login.interceptor.LoginInterceptor;
+import com.trio.pintree.login.interceptor.RequestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,16 +10,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class OauthConfig implements WebMvcConfigurer {
 
-    private final NaverOauthInterceptor oauthInterceptor;
+    private final NaverOauthInterceptor naverOauthInterceptor;
+    private final LoginInterceptor loginInterceptor;
 
-    public OauthConfig(NaverOauthInterceptor oauthInterceptor) {
-        this.oauthInterceptor = oauthInterceptor;
+    public OauthConfig(NaverOauthInterceptor naverOauthInterceptor, LoginInterceptor loginInterceptor) {
+        this.naverOauthInterceptor = naverOauthInterceptor;
+        this.loginInterceptor = loginInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(oauthInterceptor)
+        registry.addInterceptor(naverOauthInterceptor)
                 .addPathPatterns("/login/naver");
+
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/login/google")
+                .addPathPatterns("/api/login/naver")
+                .addPathPatterns("/api/login/kakao");
     }
 
 }
