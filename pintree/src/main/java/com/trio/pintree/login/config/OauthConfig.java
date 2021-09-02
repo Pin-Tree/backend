1b6ace3b68c0ea13da92eb882270b8a198eb3710
woody <woojihye2339@gmail.com>
2021-09-02 15:56:17 +0900

    fix : LoginInterceptor 클래스 삭제
    > LoginInterceptor 삭제
      - 로그인 과정에서는 JWT 확인하는 로직은 필요하지 않음
    
    issue : #21

--- pintree/src/main/java/com/trio/pintree/login/config/OauthConfig.java
+++ pintree/src/main/java/com/trio/pintree/login/config/OauthConfig.java
@@ -1,8 +1,6 @@
 package com.trio.pintree.login.config;
 
 import com.trio.pintree.login.interceptor.NaverOauthInterceptor;
-import com.trio.pintree.login.interceptor.LoginInterceptor;
-import com.trio.pintree.login.interceptor.RequestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,22 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class OauthConfig implements WebMvcConfigurer {
 
     private final NaverOauthInterceptor naverOauthInterceptor;
-    private final LoginInterceptor loginInterceptor;
 
-    public OauthConfig(NaverOauthInterceptor naverOauthInterceptor, LoginInterceptor loginInterceptor) {
+    public OauthConfig(NaverOauthInterceptor naverOauthInterceptor) {
         this.naverOauthInterceptor = naverOauthInterceptor;
-        this.loginInterceptor = loginInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(naverOauthInterceptor)
-                .addPathPatterns("/login/naver");
-
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/api/login/google")
-                .addPathPatterns("/api/login/naver")
-                .addPathPatterns("/api/login/kakao");
+                .addPathPatterns("/auth/social/redirect/naver");
     }
 
 }
