4e7ba87fa57a37b50477fffd5f4f88664bc03dba
woody <woojihye2339@gmail.com>
2021-09-24 18:17:23 +0900

    feature : Node 생성자 추가 및 팩토리 메소드 생성
    > Node 팩토리 메소드 생성
      - createMainNode() : parent 노드를 받아오지 않아도 됨
      - createNonMainNode()
    
    > Node private 생성자 구현
      - Node 생성에 반드시 필요한 필드만 파라미터로
    
    > equals() hashCode() 메소드 재정의
      - 엔티티는 id만 같다면 동일한 엔티티로 취급
    
    > index long 타입으로 변경
      - 100 단위로 증가하기 때문에 int보다 큰 단위인 long으로 변경
    
    issue : #40

--- pintree/src/main/java/com/trio/pintree/login/domain/Node.java
+++ pintree/src/main/java/com/trio/pintree/login/domain/Node.java
@@ -3,18 +3,20 @@ package com.trio.pintree.login.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
+@Table(name = "node")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Node {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "node_id")
     private Long id;
 
@@ -25,7 +27,7 @@ public class Node {
     private boolean isUp;
 
     @Column(name = "node_index")
-    private int index;
+    private long index;
 
     @Column(name = "node_memo")
     private String memo;
@@ -39,9 +41,45 @@ public class Node {
     @OneToMany(mappedBy = "child", cascade = CascadeType.ALL)
     private List<ParentChild> children = new ArrayList<>();
 
+    private Node(boolean isMain, boolean isUp, long index, boolean isOfficial) {
+        this.isMain = isMain;
+        this.isUp = isUp;
+        this.index = index;
+        this.isOfficial = isOfficial;
+    }
 
-    @OneToMany(mappedBy = "child")
-    private List<Children> children = new ArrayList<>();
+    public static Node createMainNode(boolean isUp, long index, boolean isOfficial) {
+        return new Node(false, isUp, index, isOfficial);
+    }
 
+    public static Node createNonMainNode(Node parent, boolean isUp, long index, boolean isOfficial) {
+        Node node = new Node(false, isUp, index, isOfficial);
+        ParentChild parentChild = ParentChild.create(parent, node);
+        parent.addChild(parentChild);
+        node.addParent(parentChild);
+
+        return node;
+    }
+
+    private void addChild(ParentChild parentChild) {
+        children.add(parentChild);
+    }
+
+    private void addParent(ParentChild parentChild) {
+        this.parent = parentChild;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return id.equals(node.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
 
