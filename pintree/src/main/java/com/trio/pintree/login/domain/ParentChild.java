6fafaa81b4122d00e060de0adfe7768ccdbfae21
woody <woojihye2339@gmail.com>
2021-09-24 18:18:38 +0900

    feature : ParentChild 생성자 추가 및 팩토리 메소드 생성
    > ParentChild 팩토리 메소드 create() 구현
    
    > private 생성자 구현
      - ParentChild 생성에 반드시 필요한 필드만 파라미터로
    
    > equals() hashCode() 메소드 재정의
      - 엔티티는 id만 같다면 동일한 엔티티로 취급
    
    issue : #40

--- pintree/src/main/java/com/trio/pintree/login/domain/ParentChild.java
+++ pintree/src/main/java/com/trio/pintree/login/domain/ParentChild.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
+@Table(name = "parent_child")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ParentChild {
 
@@ -24,4 +26,25 @@ public class ParentChild {
     @JoinColumn(name = "child_id")
     private Node child;
 
+    private ParentChild(Node parent, Node child) {
+        this.parent = parent;
+        this.child = child;
+    }
+
+    public static ParentChild create(Node parent, Node child) {
+        return new ParentChild(parent, child);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParentChild that = (ParentChild) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
