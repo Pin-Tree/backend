76ca02593834eb828298bd582858aa155f9e2b69
woody <woojihye2339@gmail.com>
2021-09-25 16:20:36 +0900

    test : NodeController 테스트 코드 구현
    > NodeControllerTest 구현
      - @WebMvcTest 테스트
    
    > 노드 생성 테스트 코드 추가
      - 오피셜노드를_생성한다()
      - 커스텀노드를_생성한다()
    
    > 현재 테스트 코드는 HTTP 상태 정보만 확인
      - 추후 필드 확인 등 테스트 코드 추가 필요
    
    issue : #41

---
+++ pintree/src/test/java/com/trio/pintree/login/controller/NodeControllerTest.java
@@ -0,0 +1,88 @@
+package com.trio.pintree.login.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trio.pintree.login.domain.Node;
+import com.trio.pintree.login.dto.node.CreateCustomNodeRequest;
+import com.trio.pintree.login.dto.node.CreateNodeResponse;
+import com.trio.pintree.login.dto.node.CreateOfficialNodeRequest;
+import com.trio.pintree.login.service.NodeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(NodeController.class)
+class NodeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @MockBean
+    private NodeService nodeService;
+
+    @Test
+    public void 오피셜노드를_생성한다() throws Exception {
+        // given
+        final String name = "HTML";
+        final Long parentId = null;
+        final boolean isMain = true;
+        final boolean isUp = true;
+        final long index = 100L;
+        final Long officialInfoId = null;
+        CreateOfficialNodeRequest request =
+                new CreateOfficialNodeRequest(name, parentId, isMain, isUp, index, officialInfoId);
+
+        final boolean isOfficial = true;
+        Node createdOfficialNode = Node.createMainNode(name, index, isOfficial);
+
+        // when
+        when(nodeService.createOfficialNode(request))
+                .thenReturn(CreateNodeResponse.from(createdOfficialNode));
+        // then
+        this.mockMvc.perform(
+                post("/api/node/official")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void 커스텀노드를_생성한다() throws Exception {
+        // given
+        final String name = "HTML";
+        final Long parentId = null;
+        final boolean isMain = true;
+        final boolean isUp = true;
+        final long index = 100L;
+        final String customInfo = "HTML(Hypertext Markup Language)은 웹 페이지와 그 내용 즉, 컨텐츠의 구조를 정의하는 마크업 언어입니다.";
+
+        CreateCustomNodeRequest request =
+                new CreateCustomNodeRequest(name, parentId, isMain, isUp, index, customInfo);
+
+        final boolean isOffical = false;
+        Node createdCustomNode = Node.createMainNode(name, index, isOffical);
+
+        // when
+        when(nodeService.createCustomNode(request))
+                .thenReturn(CreateNodeResponse.from(createdCustomNode));
+
+        // then
+        this.mockMvc.perform(
+                post("/api/node/custom")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+        ).andExpect(status().isOk());
+    }
+
+}
