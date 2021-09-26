f59b7cfae8759ab009a1a6fd0472bd02d9e40e93
pbg0205 <pbg0205@naver.com>
2021-09-26 23:12:51 +0900

    feat : session db로 부터 사용자 UUID 전달 로직 완성 1. LoginMemberArgumentResolver 작성 (1) supportsParameter : resolveArgument이 작동 가능 여부를 확인하는 메서드.    - parameter type(UUID)과 @Login annotaion 포함 여부를 확인.
    (2) resolveArgument : header 내부에 JWT를 추출 및 복호화해 MemberId를 전달.
    
    2. NotMatchedUserFoundException 생성
    (1) 복호화된 Jwt의 MemberId와 Session DB의 ID가 불일치 시 발생하는 예외.

---
+++ pintree/src/main/java/com/trio/pintree/login/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,58 @@
+package com.trio.pintree.login.resolver;
+
+import com.trio.pintree.login.exception.NotMatchedUserException;
+import com.trio.pintree.login.repository.AuthRepository;
+import com.trio.pintree.login.util.JwtUtil;
+import com.trio.pintree.login.util.JwtValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final AuthRepository authRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.debug("LoginMemberArgumentResolver 실행");
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean isUUIDType = UUID.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && isUUIDType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+
+        log.debug("LoginMemberArgumentResolver.resolveArgument");
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String headerAsString = request.getHeader(HttpHeaders.AUTHORIZATION);
+        JwtValidator.validateJwtHeader(headerAsString);
+
+        String uuid = extractMemberIdFromEncodedJwt(headerAsString);
+        log.debug("uuid : {}", uuid);
+
+        if (!authRepository.existsById(uuid)) {
+            throw new NotMatchedUserException();
+        }
+        return UUID.fromString(uuid);
+    }
+
+    private String extractMemberIdFromEncodedJwt(String headerAsString) {
+        String[] split = headerAsString.split(" ");
+        String jwt = split[1];
+        return JwtUtil.decodeJwt(jwt);
+    }
+}
