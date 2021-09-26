be15dfac9ca6421fe9bfc43434766d32421c903c
pbg0205 <pbg0205@naver.com>
2021-09-26 23:13:16 +0900

    feat : LoginMemberArgumentResolver 등록

---
+++ pintree/src/main/java/com/trio/pintree/login/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.trio.pintree.login.config;
+
+import com.trio.pintree.login.resolver.LoginMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+}
