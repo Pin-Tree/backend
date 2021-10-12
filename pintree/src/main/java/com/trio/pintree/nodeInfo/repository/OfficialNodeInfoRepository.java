b59c6369defa735bf8f75473d2b499761810bdc6
sanhee <68936@naver.com>
2021-10-12 17:59:13 +0900

    feat: BookInfoService save(), find() 정의

--- pintree/src/main/java/com/trio/pintree/nodeInfo/repository/OfficialNodeInfoRepository.java
+++ pintree/src/main/java/com/trio/pintree/nodeInfo/repository/OfficialNodeInfoRepository.java
@@ -3,6 +3,8 @@ package com.trio.pintree.nodeInfo.repository;
 import com.trio.pintree.nodeInfo.domain.OfficialNodeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OfficialNodeInfoRepository<T extends OfficialNodeInfo> extends JpaRepository<T, Long> {
+import java.util.List;
 
+public interface OfficialNodeInfoRepository<T extends OfficialNodeInfo> extends JpaRepository<T, Long> {
+    List<T> findByNodeId(Long nodeId);
 }
---
+++ pintree/src/main/java/com/trio/pintree/nodeInfo/service/BookInfoService.java
@@ -0,0 +1,30 @@
+package com.trio.pintree.nodeInfo.service;
+
+import com.trio.pintree.nodeInfo.domain.BookInfo;
+import com.trio.pintree.nodeInfo.dto.BookInfoDto;
+import com.trio.pintree.nodeInfo.dto.BookInfoRequestDto;
+import com.trio.pintree.nodeInfo.dto.BookInfosDto;
+import com.trio.pintree.nodeInfo.repository.BookInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BookInfoService {
+    private final BookInfoRepository repository;
+
+    public void save(Long nodeId, BookInfoRequestDto infoDto) {
+        BookInfo info = BookInfoRequestDto.of(nodeId, infoDto);
+        repository.save(info);
+    }
+
+    public BookInfosDto find(Long nodeId) {
+        List<BookInfo> bookInfoList = repository.findByNodeId(nodeId);
+        List<BookInfoDto> bookInfoListDto = bookInfoList.stream().map(BookInfoDto::from).collect(Collectors.toList());
+        return BookInfosDto.from(bookInfoListDto);
+    }
+}
+
