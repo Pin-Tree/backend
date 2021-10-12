acd16e90a36ad054ce509246a376dce2955fca8d
sanhee <68936@naver.com>
2021-10-12 18:51:34 +0900

    style: OfficialNodeInfoRepository 문맥 정렬

--- pintree/src/main/java/com/trio/pintree/nodeInfo/repository/OfficialNodeInfoRepository.java
+++ pintree/src/main/java/com/trio/pintree/nodeInfo/repository/OfficialNodeInfoRepository.java
@@ -2,11 +2,14 @@ package com.trio.pintree.nodeInfo.repository;
 
 import com.trio.pintree.nodeInfo.domain.OfficialNodeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OfficialNodeInfoRepository<T extends OfficialNodeInfo> extends JpaRepository<T, Long> {
     List<T> findByNodeId(Long nodeId);
+
     Optional<T> findByNodeIdAndInfoId(Long nodeId, Long infoId);
 
     @Transactional
